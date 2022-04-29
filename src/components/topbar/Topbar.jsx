f5219878b35956e7530f315f50adc333524b8b68
bhogalprabjot <bhogalprabjot1@gmail.com>
2022-04-29 08:29:26 +0530

    Nav bar and side menu complete

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,14 +1,19 @@
 import './Topbar.scss';
-
-const Topbar = () => {
+import { MdContactMail } from 'react-icons/md';
+const Topbar = ({ menuOpen, setMenuOpen }) => {
     return (
-        <div className='topbar'>
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href='#intro'>Prabjot</a>
+                    <a href='#intro' className='logo'>P. Bhogal</a>
                 </div>
                 <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
 
+                    </div>
                 </div>
             </div>
         </div>

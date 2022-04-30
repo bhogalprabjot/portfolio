9cd2089c18385755730be54bd11e2ed02b6aeb51
bhogalprabjot <bhogalprabjot1@gmail.com>
2022-04-30 08:41:20 +0530

    Base Intro Page done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,22 @@
 import './Intro.scss';
-
+import { AiOutlineDown } from 'react-icons/ai';
 const Intro = () => {
     return (
-        <div className='intro' id='intro'>Intro</div>
+        <div className='intro' id='intro'>
+            {/* <div className="left">
+
+            </div> */}
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Prabjot Singh Bhogal</h1>
+                    <h3>Software Engineer</h3>
+                </div>
+                <a href="#portfolio">
+                    <AiOutlineDown />
+                </a>
+            </div>
+        </div>
     )
 }
 
